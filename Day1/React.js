9c55333655b1f232367ef2d57345d7f8bd2a8c5e
Gargi Chakraborty <101615476+Gargi008@users.noreply.github.com>
2023-03-29 13:29:49 +0530

    Create React.js

---
+++ Day1/React.js
@@ -0,0 +1,81 @@
+//React a JS library
+
+
+/* browser --> server 
+html css js 
+
+we had to accomodate JS to work with different browsers
+
+//jQuery - th syntax works across all the browsers
+
+//backbone.js  - organising files
+
+
+//ajax - we started focusing on JS rather than HTML
+//ajax - our apps r like desktop apps and JS updates the DOM 
+
+SPA - Single page applications 
+
+
+2010 - Angular  - Google --- large containers to wrap our project
+
+MVC - model view control 
+
+
+it became very complex to find bugs 
+
+
+2013 facebook came up with React 
+React developed a whole new way of building apps.
+
+
+Uber , insta , netflix, paypal , pinterest
+
+
+*/
+
+
+// Imparative vs . Declarative
+
+// In imperative paradigm we directly change indivisual patts of our app 
+// in response to various user events 
+
+
+// It takes a lot of time for DOM chnages to happen
+
+
+
+
+//1. Declarative approach  
+
+/* let State= {
+    user: 'Gargi',
+    isLoggedIn: true,
+} */
+
+
+//React asks how ur app should look like,
+
+//I ll make the chnages for u 
+
+//2. Build websites with reusable components ( JS function)
+
+/* function Hello(){
+    return <h1>Helloooo </h1>
+}
+ */
+/* class Hello extends React.component{
+    render(){
+       return <h1>Helloooo </h1>
+}
+      
+    }
+}
+ */
+
+/* 3. unidirectional data flow */
+
+
+//4. UI Library 
+/* React 
+ReactDOM */
