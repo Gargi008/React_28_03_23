0d66b8326b8a47362c998d032e28a8662d8695cc
Gargi Chakraborty <101615476+Gargi008@users.noreply.github.com>
2023-04-05 12:10:00 +0530

    Add files via upload

---
+++ srcDay7/App.js
@@ -0,0 +1,67 @@
+import {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+//jsx is syntax entension ojf JS
+// it allows us to write something that looks like HTNL inside JS
+class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      usersData: [],
+      searchText: '',
+   
+    }
+   //console.log(" I am one :)")
+  }
+      /* mount - firsttime a component gets placed onto the DOM */
+  componentDidMount() {
+   // console.log("I am three :)")
+    fetch("https://jsonplaceholder.typicode.com/users/")
+    .then((response) => response.json())
+    .then((users) => this.setState(()=>{
+      return {usersData : users}
+    }))
+
+   }
+   onSearch=(event)=>{
+    const searchText= event.target.value.toLocaleLowerCase()
+    this.setState(()=>{
+      return {searchText}// 2
+    })
+   }
+
+ 
+  render(){
+    //console.log("I am two :)")
+     //key props
+
+     
+
+
+     const filteredUsers= this.state.usersData.filter((user)=>{ // 10
+      return user.name.toLocaleLowerCase().includes(this.state.searchText)
+    }) 
+    return (
+      <div className="App">
+           <input 
+           className="searchBox"
+           type="search"
+           placeholder='search users here'
+           onChange={this.onSearch}
+           />
+        {
+        filteredUsers.map((user,id)=>{
+            return <h1 key={user.id}>{user.name}</h1>
+
+          })
+
+        }
+      </div>
+      
+    
+    );
+  }
+ 
+}
+
+export default App;
